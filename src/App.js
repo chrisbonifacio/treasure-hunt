9a899047057603ae8638efd982155ec85fc56754
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-03-02 22:58:05 -0500

    setup state and helper functions for movement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
-function App() {
+import axiosWithAuth from "./utils/axiosWithAuth"
+import Controls from "./components/Controls"
+
+export default function App() {
+  const [player, setPlayer] = useState({})
+  const [map, setMap] = useState({ 0: { n: "?", s: "?", e: "?", w: "?" } })
+
+  // initialize player
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/adv/init")
+      .then(res => {
+        setPlayer(res.data)
+        return res.data
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log("Player:", player)
+  console.log("Map:", map)
+
+  // graph map
+  function graph(prevRoom, nextRoom, direction) {
+    let opposite = {
+      n: "s",
+      s: "n",
+      e: "w",
+      w: "e"
+    }
+
+    let newMap = { ...map }
+
+    newMap[prevRoom][direction] = nextRoom
+
+    if (!Object.keys(newMap).includes(nextRoom)) {
+      newMap[nextRoom] = {}
+
+      for (let exit in player.exits) {
+        newMap[nextRoom][exit] = "?"
+      }
+    }
+
+    newMap[nextRoom][opposite[direction]] = prevRoom
+
+    setMap(newMap)
+  }
+
+  // move
+  async function move(direction) {
+    if (player.exits.includes(direction)) {
+      try {
+        const prevRoom = player.room_id
+
+        const response = await axiosWithAuth().post("/adv/move", {
+          direction: direction
+        })
+
+        setPlayer(response.data)
+
+        const nextRoom = response.data.room_id
+
+        graph(prevRoom, nextRoom, direction)
+      } catch (error) {
+        console.log("Move Error:", error)
+      }
+    } else {
+      console.log("Cannot move in that direction")
+    }
+  }
+
+  // take treasure
+  async function take(treasure) {
+    try {
+      await axiosWithAuth().post("/adv/take", { name: treasure })
+    } catch (error) {
+      console.log("Take Error:", error)
+    }
+  }
+
+  // drop treasure
+  async function drop(treasure) {
+    try {
+      await axiosWithAuth().post("/adv/drop", { name: treasure })
+    } catch (error) {
+      console.log("Drop Error:", error)
+    }
+  }
+
+  // sell treasure
+  async function sell(treasure) {
+    try {
+      await axiosWithAuth().post("/adv/sell", { name: treasure })
+    } catch (error) {
+      console.log("Sell Error:", error)
+    }
+  }
+
+  // check inventory
+  async function inventory() {
+    try {
+      const inventory = await axiosWithAuth().post("/adv/sell")
+      console.log(inventory)
+    } catch (error) {
+      console.log("Inventory Error:", error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {Object.keys(player).map(key => {
+        return (
+          <p>
+            {key} : {player[key]}`
+          </p>
+        )
+      })}
 
-export default App;
+      <Controls move={move} take={take} drop={drop} />
+    </div>
+  )
+}
