53945e509ad097643f91bf5dcc63a17c25e24cd3
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-03-05 15:07:38 -0500

    render Inventory

---
+++ src/components/Inventory.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import axiosWithAuth from "../utils/axiosWithAuth"
+import "./Inventory.scss"
+
+export default function Inventory({ sell }) {
+  const dispatch = useDispatch()
+  const status = useSelector(state => state.inventory)
+
+  async function getInventory() {
+    try {
+      const response = await axiosWithAuth().post("/adv/status")
+      dispatch({ type: "GET_INVENTORY", payload: response.data })
+    } catch (error) {
+      console.log("Inventory Error:", error)
+    }
+  }
+
+  return (
+    <div className="inventory">
+      <h2>Status</h2>
+      <button onClick={getInventory}>Check Inventory</button>
+      <ul>
+        {Object.keys(status).map(key => {
+          if (key !== "inventory") {
+            return (
+              <li>
+                {key}:{status[key]}
+              </li>
+            )
+          }
+        })}
+      </ul>
+      <h2>Inventory</h2>
+
+      <ul>
+        {status.inventory.map(item => {
+          return (
+            <li>
+              {item}
+              <button onClick={() => sell(item)}>sell</button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
